26a3c94e54c39eb7be4e3425be908b97b1c156ee
hinako <hinako@ndg-no-mbp>
2020-10-14 17:16:12 +0900

    テスト文追記

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,11 @@ public class Main {
 		//インスタンス化
 		Werewolf ww = new Werewolf("狼男", 150);
 		Werewolf ww2 = new Werewolf();
+		Hero h = new Hero("スラリン", 200);
 		//テスト
 		System.out.println("name:" + ww.name + "　hp:" + ww.hp);
 		System.out.println("name:" + ww2.name + "　hp:" + ww2.hp);
+		ww.attack(h);
 
 	}
 
